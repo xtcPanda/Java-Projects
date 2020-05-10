cbcc477329ee32f7793b1e135ff0357d9bd3bc7e
Moaaz Akram Alhajj <41624372+xtcPanda@users.noreply.github.com>
2020-05-10 18:21:30 +0200

    adding some comments

--- Shipment/src/Shipment/FrameLayout.java
+++ Shipment/src/Shipment/FrameLayout.java
@@ -5,14 +5,21 @@
  */
 package Shipment;
 
+//importing the lib we need
 import javax.swing.JFrame;
 
-
+//editing the JFrame class in a child class
 public class FrameLayout extends JFrame {
+    //adding a constructor for the class
     public FrameLayout(){
-        super("Hello World !");
+        //calling the constructor for the parent class with super
+        //adding a title to it
+        super("Shipment Company GUI");
+        //setting the size for the window
         setSize(600,600);
+        //configuring the exit btn
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //setting the program window to visible
         setVisible(true);
     }
 
--- Shipment/src/Shipment/shimpment.java
+++ Shipment/src/Shipment/shimpment.java
@@ -5,6 +5,7 @@
  */
 package Shipment;
 
+//importing the lib we need
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
@@ -19,13 +20,17 @@ public class shimpment{
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //to prevent some issues from happening
+        //we dont need it but some says its the best way to make a wing gui
         SwingUtilities.invokeLater(new Runnable(){
             @Override
             public void run() {
+                //we can use the code normally like this
 //               JFrame frame = new JFrame("Hello World !");
-//               frame.setSize(600,600);
+//               frame.setSize(-,-);
 //               frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 //               frame.setVisible(true);
+                //calling the constructor for the layout we made in another class
                 new FrameLayout();
             }
         
