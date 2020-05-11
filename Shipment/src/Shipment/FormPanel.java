8e15b160367152ec1322c1674aedf8fdd631093c
Moaaz Akram Alhajj <41624372+xtcPanda@users.noreply.github.com>
2020-05-11 20:49:28 +0200

    Update FormPanel.java

--- Shipment/src/Shipment/FormPanel.java
+++ Shipment/src/Shipment/FormPanel.java
@@ -6,8 +6,14 @@
 package Shipment;
 
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.TextField;
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.border.Border;
 
 /**
@@ -15,13 +21,37 @@ import javax.swing.border.Border;
  * @author mo3az
  */
 public class FormPanel extends JPanel{
+    private JLabel nameLabel;
+    private JLabel occupationLabel;
+    private JTextField nameField;
+    private JTextField occupationField;
+    private JButton okbtn;
+    
     public FormPanel(){
+        setBorder(BorderFactory.createEtchedBorder());
         Dimension dim = getPreferredSize();
         dim.width = 250;
         setPreferredSize(dim);
         
+        nameLabel = new JLabel("Name: ");
+        occupationLabel = new JLabel("Occupation: ");
+        nameField = new JTextField(10);
+        occupationField = new JTextField(10);
+        
+        okbtn = new JButton("OK");
+        
+        
         Border insideBorder = BorderFactory.createTitledBorder("Add Person");
         Border outsideBorder = BorderFactory.createEmptyBorder(5,5,5,5);
         setBorder(BorderFactory.createCompoundBorder(outsideBorder, insideBorder));
+        
+        setLayout(new GridBagLayout());
+        
+        GridBagConstraints gc = new GridBagConstraints();
+        gc.gridx = 0;
+        gc.gridy = 0;
+        gc.weightx = 1;
+        gc.weighty = 1;
+        gc.fill = GridBagConstraints.NONE;
     }
 }
