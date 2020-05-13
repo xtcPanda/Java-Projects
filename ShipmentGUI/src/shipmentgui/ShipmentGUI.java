bf74d53abd4c88b005fed20e83a6dd3e24774cb6
Moaaz Akram Alhajj <41624372+xtcPanda@users.noreply.github.com>
2020-05-13 15:11:40 +0200

    update

--- ShipmentGUI/src/shipmentgui/ShipmentGUI.java
+++ ShipmentGUI/src/shipmentgui/ShipmentGUI.java
@@ -5,17 +5,29 @@
  */
 package shipmentgui;
 
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+
 /**
  *
  * @author mo3az
  */
-public class ShipmentGUI {
+public class ShipmentGUI{
 
+    public ShipmentGUI(){
+        JFrame frame = new JFrame("Shipment CO. GUI");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setLayout(new BorderLayout(0,10));
+        frame.setSize(500,500);
+        frame.setLocation(600, 200);
+
+        frame.setVisible(true);
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        new ShipmentGUI();
     }
     
 }
